d2eaeaaf2e248b4cbf8dca83f9313a13d916026c
Zhangyao719 <1258858403@qq.com>
2021-08-22 22:34:14 +0800

    feat: add promise.js

---
+++ 6 手写Promise/lib/Promise.js
@@ -0,0 +1,42 @@
+// es5
+(function (window) {
+  function Promise(excutor) {
+    /**
+     * 
+     */
+  }
+
+  Promise.prototype.then = function (onResolve, onReject) {
+    
+  }
+
+  Promise.prototype.catch = function (onReject) {
+    
+  }
+
+  Promise.resolve = function (params) {
+    
+  }
+
+  Promise.reject = function (params) {
+    
+  }
+
+  /**
+   * @param {Array} promises promise数组
+   * @return {Promise}
+   */
+  Promise.all = function (promises) {
+    
+  }
+
+  /**
+   * @param {Array} promises promise数组
+   * @return {Promise}
+   */
+  Promise.race = function (promises) {
+    
+  }
+
+  window.Promise = Promise
+})(window)
